b52f924a043a99adeb76c0dd779b89d53d8e741b
arshia <arshiarezagholi1212@gmail.com>
2024-07-24 19:37:17 +0330

    v1 views implementation

--- backend/blog/views/crud_views/v1_views.py
+++ backend/blog/views/crud_views/v1_views.py
@@ -1,5 +1,24 @@
+from django.views import View
+
 """
 
 Django CRUD API
 
 """
+
+
+class CrudPostView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def patch(self, request):
+        pass
+
+    def delete(self, request):
+        pass
