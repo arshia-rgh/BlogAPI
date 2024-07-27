0f9c5940167304aa1dbfc3c353c8b920ed89808b
arshia <arshiarezagholi1212@gmail.com>
2024-07-27 12:35:28 +0330

    added more docstrings

--- backend/blog/views/crud_views/v1_views.py
+++ backend/blog/views/crud_views/v1_views.py
@@ -12,6 +12,10 @@ Django CRUD API
 
 
 def serialize_post(post: Post):
+    """
+    custom serializer for handle FileField serializing
+
+    """
     post_dict = model_to_dict(post)
     for field in post._meta.fields:
         if isinstance(getattr(post, field.name), FieldFile):
@@ -22,8 +26,16 @@ def serialize_post(post: Post):
 class CrudPostView(View):
     def get(self, request, *args, **kwargs):
         """
-        If the pk has given returns only one object (DetailView)
-        else returns all objects
+            Handles GET requests. If a primary key (pk) is provided in the URL, returns the details of the specific post.
+            Otherwise, returns a list of all posts.
+
+            Parameters:
+            request (HttpRequest): The HTTP request object.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+            Returns:
+            JsonResponse: A JSON response containing th
         """
         pk = self.kwargs.get("pk")
         if not pk:
@@ -33,10 +45,10 @@ class CrudPostView(View):
                 obj_dict = serialize_post(obj)
                 data[f'post_{obj.id}'] = obj_dict
             return JsonResponse(data)
+
         obj = Post.objects.get(pk=pk)
         obj_dict = serialize_post(obj)
         return JsonResponse(obj_dict)
-
 
     def post(self, request, *args, **kwargs):
         pass
