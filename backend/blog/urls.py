e80b4ee938500d4f7da12a8cd3e4e1900da9cfc5
arshia <arshiarezagholi1212@gmail.com>
2024-07-27 12:18:45 +0330

    added get for post crud

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views.crud_views.v1_views import CrudPostView
 urlpatterns = [
-
+    path('posts/', CrudPostView.as_view(), name="posts"),
 ]
--- backend/blog/views/crud_views/v1_views.py
+++ backend/blog/views/crud_views/v1_views.py
@@ -1,4 +1,8 @@
+from django.db.models.fields.files import FieldFile
+from django.forms import model_to_dict
+from django.http import JsonResponse
 from django.views import View
+from ...models import Post
 
 """
 
@@ -7,18 +11,31 @@ Django CRUD API
 """
 
 
+def serialize_post(post: Post):
+    post_dict = model_to_dict(post)
+    for field in post._meta.fields:
+        if isinstance(getattr(post, field.name), FieldFile):
+            post_dict[field.name] = getattr(post, field.name).url if getattr(post, field.name) else None
+    return post_dict
+
+
 class CrudPostView(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
+        query = Post.objects.all()
+        data = {}
+        for obj in query:
+            obj_dict = serialize_post(obj)
+            data[f'{obj.id}'] = obj_dict
+        return JsonResponse(data)
+
+    def post(self, request, *args, **kwargs):
         pass
 
-    def post(self, request):
+    def put(self, request, *args, **kwargs):
         pass
 
-    def put(self, request):
+    def patch(self, request, *args, **kwargs):
         pass
 
-    def patch(self, request):
-        pass
-
-    def delete(self, request):
+    def delete(self, request, *args, **kwargs):
         pass
